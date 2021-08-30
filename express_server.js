6bd106c71509efafa16610e2146fee07948a2ff5
Zak Warsame <zakwarsamee@gmail.com>
2021-08-30 23:51:19 +0000

    implement redirects

--- express_server.js
+++ express_server.js
@@ -7,13 +7,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  // create a key value pair and put them in the urlDatabase (a new short url is generated, longURL is coming from the POST made by a form)
+  const newShortUrl = generateRandomString();
+  urlDatabase[newShortUrl] = req.body.longURL;
+  res.redirect(`/urls/${newShortUrl}`);
 });
 
 app.get("/", (req, res) => {
@@ -28,6 +30,7 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -40,13 +43,28 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// URL REDIRECTS
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if (!longURL){
+    res.status(404).send({ error: 'Short URL not found!' })
+  }
+  res.redirect(longURL);
+});
+
+// FUNCTIONS
+
 function generateRandomString() {
-    //store all alphanumerics in a string
-    const alphanumerics ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
-    // use spreader to create an array with 6 empty elements
-    // loop through them and generate a random number number each time (use 0 otherwise)
-    // store that number and pass it as an index within the alphanumerics string
-    // join the characters
-    return [...Array(6)].map(i=>alphanumerics[Math.random()*alphanumerics.length|0]).join('');
+  //store all alphanumerics in a string
+  const alphanumerics =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  // use spreader to create an array with 6 empty elements
+  // loop through them and generate a random number number each time (use 0 otherwise)
+  // store that number and pass it as an index within the alphanumerics string
+  // join the characters
+  return [...Array(6)]
+    .map((i) => alphanumerics[(Math.random() * alphanumerics.length) | 0])
+    .join("");
 }
