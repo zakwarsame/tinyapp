af2c3e9a77751f566168ea587dcd8fdf63185f7c
Zak Warsame <zakwarsamee@gmail.com>
2021-08-30 20:55:45 +0000

    fill out urls_index.ejs with a basic html skeleton and looped over url database

--- express_server.js
+++ express_server.js
@@ -13,8 +13,13 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Hello!");
   });
 
   app.get("/hello", (req, res) => {
