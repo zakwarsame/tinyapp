b5abcdbd1573aec5a0722133a8a2aab9bf53093a
Zak Warsame <zakwarsamee@gmail.com>
2021-08-30 23:20:39 +0000

    install body-parser to make POST requests readable. Create a urls/new route for post submissions from the form:

--- express_server.js
+++ express_server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,15 +25,28 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-    res.render("urls_show", templateVars);
-  });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+    //store all alphanumerics in a string
+    const alphanumerics ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    // use spreader to create an array with 6 empty elements
+    // loop through them and generate a random number number each time (use 0 otherwise)
+    // store that number and pass it as an index within the alphanumerics string
+    // join the characters
+    return [...Array(6)].map(i=>alphanumerics[Math.random()*alphanumerics.length|0]).join('');
+}
