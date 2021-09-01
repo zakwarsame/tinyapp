c7785fe96a59c5de5ee179ac31be9f4cea7e98c1
Zak Warsame <zakwarsamee@gmail.com>
2021-09-01 00:22:33 +0000

    use cookie parser to add login and logout functionality on a new branch

--- express_server.js
+++ express_server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
@@ -23,16 +25,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
+    username: req.cookies["username"],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
@@ -43,28 +52,37 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect(`/urls/`);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect(`/urls/`);
+});
+
 // URL REDIRECTS
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  if (!longURL){
-    res.status(404).send({ error: 'Short URL not found!' })
+  if (!longURL) {
+    res.status(404).send({ error: "Short URL not found!" });
   }
   res.redirect(longURL);
 });
 
-app.post('/urls/:shortURL/delete', (req, res)=>{
-    delete urlDatabase[req.params.shortURL]
-    res.redirect('/urls')
-
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 // UPDATING the database
 
-app.post('/urls/:shortURL', (req, res)=>{
-    console.log(req.params.shortURL, req.body);
-    urlDatabase[req.params.shortURL] = req.body.longURL
-    res.redirect('/urls');
+app.post("/urls/:shortURL", (req, res) => {
+  console.log(req.params.shortURL, req.body);
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect("/urls");
 });
 
 // FUNCTIONS
