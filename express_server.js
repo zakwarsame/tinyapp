d879e0c350e5617f1a23e3d696e1c9fdb0b176a9
Zak Warsame <zakwarsamee@gmail.com>
2021-09-01 21:16:12 +0000

    create a user object that acts as a db

--- express_server.js
+++ express_server.js
@@ -13,7 +13,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-
+const users = { 
+    "userRandomID": {
+      id: "userRandomID", 
+      email: "user@example.com", 
+      password: "purple-monkey-dinosaur"
+    },
+   "user2RandomID": {
+      id: "user2RandomID", 
+      email: "user2@example.com", 
+      password: "dishwasher-funk"
+    }
+  }
+  
 
 
 
